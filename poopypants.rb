e88afc9146a28ea2a9e67c5945211c344e313a82
Eli Duke <elimduke@gmail.com>
2013-09-23 18:36:42 -0700

    streamlined the script with a hash instead of an array of arrays.

--- poopypants.rb
+++ poopypants.rb
@@ -36,20 +36,19 @@ get '/:first/:last' do
   if first.empty? || last.empty?
     redirect to("/")
   else
-    ### PROFESSOR POOPYPANTS NAME CHANGE-O-RUBY-SCRIPT 2000 ###
 
-    firsts  = [["A", "Stinky"], ["B", "Lumpy"], ["C", "Buttercup"], ["D", "Gidget"], ["E", "Crusty"], ["F", "Greasy"], ["G", "Fluffy"], ["H", "Cheeseball"], ["I", "Chim-Chim"], ["J", "Poopsie"], ["K", "Flunky"], ["L", "Booger"], ["M", "Pinky"], ["N", "Zippy"], ["O", "Goober"], ["P", "Doofus"], ["Q", "Slimy"], ["R", "Loopy"], ["S", "Snotty"], ["T", "Falafel"], ["U", "Dorky"], ["V", "Squeezit"], ["W", "Oprah"], ["X", "Skipper"], ["Y", "Dinky"], ["Z", "Zsa-Zsa"]]
-    middles = [["A", "Diaper"], ["B", "Toilet"], ["C", "Giggle"], ["D", "Bubble"], ["E", "Girdle"], ["F", "Barf"], ["G", "Lizard"], ["H", "Waffle"], ["I", "Cootie"], ["J", "Monkey"], ["K", "Potty"], ["L", "Liver"], ["M", "Banana"], ["N", "Rhino"], ["O", "Burger"], ["P", "Hamster"], ["Q", "Toad"], ["R", "Gizzard"], ["S", "Pizza"], ["T", "Gerbil"], ["U", "Chicken"], ["V", "Pickle"], ["W", "Chuckle"], ["X", "Tofu"], ["Y", "Gorilla"], ["Z", "Stinker"]]
-    lasts   = [["A", "head"], ["B", "mouth"], ["C", "face"], ["D", "nose"], ["E", "tush"], ["F", "breath"], ["G", "pants"], ["H", "shorts"], ["I", "lips"], ["J", "honker"], ["K", "butt"], ["L", "brain"], ["M", "tushie"], ["N", "chunks"], ["O", "hiney"], ["P", "biscuits"], ["Q", "toes"], ["R", "buns"], ["S", "fanny"], ["T", "sniffer"], ["U", "sprinkles"], ["V", "kisser"], ["W", "squirt"], ["X", "humperdinck"], ["Y", "brains"], ["Z", "juice"]]
+    ### PROFESSOR POOPYPANTS NAME CHANGE-O-RUBY-SCRIPT 2000
 
-    first_letter = first[0].chr.capitalize
-    middle_letter  = last[0].chr.capitalize
-    last_letter   = last[-1].chr.capitalize
-
-    new_first   = firsts.select{|f| f[0]  == first_letter}[0][1]
-    new_middle  = middles.select{|f| f[0] == middle_letter}[0][1]
-    new_last    = lasts.select{|f| f[0]   == last_letter}[0][1]
-
+    firsts  = {"A" => "Stinky", "B" => "Lumpy", "C" => "Buttercup", "D" => "Gidget", "E" => "Crusty", "F" => "Greasy", "G" => "Fluffy", "H" => "Cheeseball", "I" => "Chim-Chim", "J" => "Poopsie", "K" => "Flunky", "L" => "Booger", "M" => "Pinky", "N" => "Zippy", "O" => "Goober", "P" => "Doofus", "Q" => "Slimy", "R" => "Loopy", "S" => "Snotty", "T" => "Falafel", "U" => "Dorky", "V" => "Squeezit", "W" => "Oprah", "X" => "Skipper", "Y" => "Dinky", "Z" => "Zsa-Zsa"}
+    middles = {"A" => "Diaper", "B" => "Toilet", "C" => "Giggle", "D" => "Bubble", "E" => "Girdle", "F" => "Barf", "G" => "Lizard", "H" => "Waffle", "I" => "Cootie", "J" => "Monkey", "K" => "Potty", "L" => "Liver", "M" => "Banana", "N" => "Rhino", "O" => "Burger", "P" => "Hamster", "Q" => "Toad", "R" => "Gizzard", "S" => "Pizza", "T" => "Gerbil", "U" => "Chicken", "V" => "Pickle", "W" => "Chuckle", "X" => "Tofu", "Y" => "Gorilla", "Z" => "Stinker"}
+    lasts   = {"A" => "head", "B" => "mouth", "C" => "face", "D" => "nose", "E" => "tush", "F" => "breath", "G" => "pants", "H" => "shorts", "I" => "lips", "J" => "honker", "K" => "butt", "L" => "brain", "M" => "tushie", "N" => "chunks", "O" => "hiney", "P" => "biscuits", "Q" => "toes", "R" => "buns", "S" => "fanny", "T" => "sniffer", "U" => "sprinkles", "V" => "kisser", "W" => "squirt", "X" => "humperdinck", "Y" => "brains", "Z" => "juice"}
+    
+    new_first   = firsts["#{first[0].upcase}"]
+    new_middle  = middles["#{last[0].upcase}"]
+    new_last    = lasts["#{last[-1].upcase}"]
+    
+    ###
+    
     erb :poopypants, :locals => { :first => first.capitalize, :last => last.capitalize, :new_first => new_first, :new_middle => new_middle, :new_last => new_last }
   end
 
