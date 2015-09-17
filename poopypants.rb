a1208327b587baac81e6b44757c4166883b14fc5
Eli Duke <elimduke@gmail.com>
2015-09-17 11:42:54 -0700

    adds facebook og support

--- poopypants.rb
+++ poopypants.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+  @og_description = "Professor Poopypants's Name 'Change-O-Chart 2000' is your one-stop shop for wacky new names!"
   erb :index
 end
 
@@ -36,12 +37,13 @@ get '/:first/:last/?' do
     firsts  = {"A" => "Stinky", "B" => "Lumpy", "C" => "Buttercup", "D" => "Gidget", "E" => "Crusty", "F" => "Greasy", "G" => "Fluffy", "H" => "Cheeseball", "I" => "Chim-Chim", "J" => "Poopsie", "K" => "Flunky", "L" => "Booger", "M" => "Pinky", "N" => "Zippy", "O" => "Goober", "P" => "Doofus", "Q" => "Slimy", "R" => "Loopy", "S" => "Snotty", "T" => "Falafel", "U" => "Dorky", "V" => "Squeezit", "W" => "Oprah", "X" => "Skipper", "Y" => "Dinky", "Z" => "Zsa-Zsa"}
     middles = {"A" => "Diaper", "B" => "Toilet", "C" => "Giggle", "D" => "Bubble", "E" => "Girdle", "F" => "Barf", "G" => "Lizard", "H" => "Waffle", "I" => "Cootie", "J" => "Monkey", "K" => "Potty", "L" => "Liver", "M" => "Banana", "N" => "Rhino", "O" => "Burger", "P" => "Hamster", "Q" => "Toad", "R" => "Gizzard", "S" => "Pizza", "T" => "Gerbil", "U" => "Chicken", "V" => "Pickle", "W" => "Chuckle", "X" => "Tofu", "Y" => "Gorilla", "Z" => "Stinker"}
     lasts   = {"A" => "head", "B" => "mouth", "C" => "face", "D" => "nose", "E" => "tush", "F" => "breath", "G" => "pants", "H" => "shorts", "I" => "lips", "J" => "honker", "K" => "butt", "L" => "brain", "M" => "tushie", "N" => "chunks", "O" => "hiney", "P" => "biscuits", "Q" => "toes", "R" => "buns", "S" => "fanny", "T" => "sniffer", "U" => "sprinkles", "V" => "kisser", "W" => "squirt", "X" => "humperdinck", "Y" => "brains", "Z" => "juice"}
-    
+
     @new_first   = firsts["#{@first[0].upcase}"]
     @new_middle  = middles["#{@last[0].upcase}"]
     @new_last    = lasts["#{@last[-1].upcase}"]
-    
+
     @subtitle = "#{@first.capitalize} #{@last.capitalize} - "
+    @og_description = "According to Professor Poopypants's Name 'Change-O-Chart 2000', #{@first.capitalize} #{@last.capitalize}'s wacky new name is #{@new_first} #{@new_middle}#{@new_last}!"
     erb :poopypants
   end
 
